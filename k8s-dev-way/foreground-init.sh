d9524f15dab801c95df8ed32e0d9977c7c733690
Guillaume Lhermenier <guillaume.lhermenier@gmail.com>
2020-07-16 21:51:15 +0200

    attempt

--- k8s-dev-way/foreground-init.sh
+++ k8s-dev-way/foreground-init.sh
@@ -1,6 +1,5 @@
-# In order to type quicker your command, you may use some aliases
-# Lets try : alias k=kubectl
-alias k=kubectl
+launch.sh
+# alias k=kubectl
 # and complete -F __start_kubectl k
-complete -F __start_kubectl k
+#complete -F __start_kubectl k
 # You can now use only k instead of kubectl and still have completion working :)
