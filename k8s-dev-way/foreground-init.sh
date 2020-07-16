c322ea2af34e6c270f1d346714ab7fd063c28e57
Guillaume Lhermenier <guillaume.lhermenier@gmail.com>
2020-07-16 21:43:59 +0200

    untest

--- k8s-dev-way/foreground-init.sh
+++ k8s-dev-way/foreground-init.sh
@@ -1,6 +1,6 @@
-echo "In order to type quicker your command, you may use some aliases."
-echo "Let's try : alias k=kubectl"
+# In order to type quicker your command, you may use some aliases
+# Lets try : alias k=kubectl
 alias k=kubectl
-echo "and complete -F __start_kubectl k"
+# and complete -F __start_kubectl k
 complete -F __start_kubectl k
-echo "You can now use only k instead of kubectl and still have completion working :)"
+# You can now use only k instead of kubectl and still have completion working :)
